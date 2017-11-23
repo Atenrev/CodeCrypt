01ee9b8553d551db0a9b9f2e36547feb2e61832a
Sergi <atenrev@gmail.com>
2017-11-23 09:02:32 +0100

    Update codeUtilities.py

--- codeUtilities.py
+++ codeUtilities.py
@@ -2,14 +2,14 @@ import sys
 import hashlib
 
 class codeUtilities:
-    # Generate the key from a string passed
+    # Generate the key from a passed string
     def generateKey (key):
         b = bytearray()
         b.extend(map(ord,key))
         sha = hashlib.sha256()
         sha.update(b)
         return sha.digest()
-    # Applies an xor operation to the data passed using the key passed
+    # Applies a xor operation to the passed data using the passed key
     def operate (data, key):
         int_data = int.from_bytes(data, sys.byteorder)
         int_key = int.from_bytes(key, sys.byteorder)
